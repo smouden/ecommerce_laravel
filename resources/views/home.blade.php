a2e179ba7d5538dcdb213ec93083e17a5c005821
saad mouden <moudensaad03@gmail.com>
2024-05-23 11:32:19 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
 @extends('base')
 
 @push('custom_css')
-<link rel="stylesheet" href="{{ secure_asset('css/linearicons.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/font-awesome.min.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/themify-icons.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/bootstrap.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/owl.carousel.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/nice-select.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/nouislider.min.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/ion.rangeSlider.css') }}" />
-<link rel="stylesheet" href="{{ secure_asset('css/ion.rangeSlider.skinFlat.css') }}" />
-<link rel="stylesheet" href="{{ secure_asset('css/magnific-popup.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('css/main.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/linearicons.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/themify-icons.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/owl.carousel.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/nice-select.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/nouislider.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/ion.rangeSlider.css') }}" />
+	<link rel="stylesheet" href="{{ secure_asset('css/ion.rangeSlider.skinFlat.css') }}" />
+	<link rel="stylesheet" href="{{ secure_asset('css/magnific-popup.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/main.css') }}">
 
 	<!-- for emoji in title -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
@@ -22,62 +22,63 @@
 
 <!-- Start Banner Area -->
 <section class="banner-area organic-breadcrumb" style="text-align: center; background: #ffa500; color: white;">
-		<div class="container">
-			<div class="breadcrumb-banner">
-				<div class="col-first">
-					<h1>Welcome to Our Storee</h1>
-					<nav>
-						<a href="shop.html"
-							style="padding: 10px 20px; background-color: #ff7f50; color: white; text-decoration: none; border-radius: 5px; font-weight: bold; font-size: 16px;">Start
-							Shopping</a>
-					</nav>
-				</div>
+	<div class="container">
+		<div class="breadcrumb-banner">
+			<div class="col-first">
+				<h1>Welcome to Our Storee</h1>
+				<nav>
+					<a href="shop.html"
+						style="padding: 10px 20px; background-color: #ff7f50; color: white; text-decoration: none; border-radius: 5px; font-weight: bold; font-size: 16px;">Start
+						Shopping</a>
+				</nav>
 			</div>
 		</div>
-	</section>
-	<!-- End Banner Area -->
+	</div>
+</section>
+<!-- End Banner Area -->
 
-	<!-- start last products -->
+<!-- start last products -->
 <section class="owl-carousel active-product-area section_gap">
-    <!-- single product slide -->
-    <div class="single-product-slider">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6 text-center">
-                    <div class="section-title">
-                        <h1>Latest Products</h1>
-                        <p>Be the first to discover the latest products from our store. Don't miss the opportunity.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <!-- single product -->
-                @foreach($latestProducts as $product)
-                <div class="col-lg-3 col-md-6">
-                    <div class="single-product">
-					<img class="img-fluid" src="{{ asset($product->picture) }}" alt="{{ $product->title_product }}" style="width: 255px; height: 270px; object-fit: cover;">
-                        <div class="product-details">
-                            <h6>{{ $product->title_product }}</h6>
-                            <div class="price">
-                                <h6>${{ $product->price_product }}</h6>
-                            </div>
-                            <div class="prd-bottom">
-                                <a href="{{ route('cart.add', $product->id) }}" class="social-info">
-                                    <span class="ti-bag"></span>
-                                    <p class="hover-text">add to bag</p>
-                                </a>
-                                <a href="{{ route('product.show', $product->id) }}" class="social-info">
-                                    <span class="lnr lnr-move"></span>
-                                    <p class="hover-text">view more</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
+	<!-- single product slide -->
+	<div class="single-product-slider">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-6 text-center">
+					<div class="section-title">
+						<h1>Latest Products</h1>
+						<p>Be the first to discover the latest products from our store. Don't miss the opportunity.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<!-- single product -->
+				@foreach($latestProducts as $product)
+					<div class="col-lg-3 col-md-6">
+						<div class="single-product">
+							<img class="img-fluid" src="{{ asset($product->picture) }}" alt="{{ $product->title_product }}"
+								style="width: 255px; height: 270px; object-fit: cover;">
+							<div class="product-details">
+								<h6>{{ $product->title_product }}</h6>
+								<div class="price">
+									<h6>${{ $product->price_product }}</h6>
+								</div>
+								<div class="prd-bottom">
+									<a href="{{ route('cart.add', $product->id) }}" class="social-info">
+										<span class="ti-bag"></span>
+										<p class="hover-text">add to bag</p>
+									</a>
+									<a href="{{ route('product.show', $product->id) }}" class="social-info">
+										<span class="lnr lnr-move"></span>
+										<p class="hover-text">view more</p>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				@endforeach
+			</div>
+		</div>
+	</div>
 </section>
 <!-- end last products -->
 
@@ -86,26 +87,21 @@
 
 
 @push('custom_js')
-	<script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
+	<script src="{{ secure_asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
 		integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
 		crossorigin="anonymous"></script>
-		<script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
-	<script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
-	<script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
-	<script src="{{ asset('js/jquery.sticky.js') }}"></script>
-	<script src="{{ asset('js/nouislider.min.js') }}"></script>
-	<script src="{{ asset('js/countdown.js') }}"></script>
-	<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
-	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+	<script src="{{ secure_asset('js/vendor/bootstrap.min.js') }}"></script>
+	<script src="{{ secure_asset('js/jquery.ajaxchimp.min.js') }}"></script>
+	<script src="{{ secure_asset('js/jquery.nice-select.min.js') }}"></script>
+	<script src="{{ secure_asset('js/jquery.sticky.js') }}"></script>
+	<script src="{{ secure_asset('js/nouislider.min.js') }}"></script>
+	<script src="{{ secure_asset('js/countdown.js') }}"></script>
+	<script src="{{ secure_asset('js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ secure_asset('js/owl.carousel.min.js') }}"></script>
 	<!--gmaps Js-->
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
-	<script src="{{ asset('js/gmaps.min.js') }}"></script>
-	<script src="{{ asset('js/main.js') }}"></script>
+	<script src="{{ secure_asset('js/gmaps.min.js') }}"></script>
+	<script src="{{ secure_asset('js/main.js') }}"></script>
+
 @endpush
-
-
-
-
-
-
