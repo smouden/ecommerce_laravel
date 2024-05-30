247ce75e709b184a0c3b5e0a0831fbd6a8447625
saad mouden <moudensaad03@gmail.com>
2024-05-30 16:48:05 +0200

    fix

--- resources/views/admin/orders/edit.blade.php
+++ resources/views/admin/orders/edit.blade.php
@@ -5,8 +5,8 @@
     <div class="row d-flex justify-content-center">
         <div class="col-sm-12 col-md-8 col-lg-6">
             <div class="bg-secondary rounded h-100 p-4">
-                <h6 class="mb-4">Modify the order status</h6>
-                <form action="{{ secure_url('/admin/orders/update', $order->id) }}" method="POST">
+                <h6 class="mb-4">Modify the Order Status</h6>
+                <form action="{{ url('/admin/orders/' . $order->id) }}" method="POST">
                     @csrf
                     @method('PUT') <!-- Important pour la mise à jour -->
                     <div class="mb-3">
@@ -19,7 +19,7 @@
                         </select>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Mettre à jour</button>
+                    <button type="submit" class="btn btn-primary">Update</button>
                 </form>
             </div>
         </div>
