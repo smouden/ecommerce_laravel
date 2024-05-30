79362837978700153e747aeec25ff33b69032eb6
saad mouden <moudensaad03@gmail.com>
2024-05-30 16:23:23 +0200

    fix

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,16 +1,15 @@
 @extends('admin.base')
 
-
 @section('content')
 <!-- Table Start -->
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
         <div class="col-12">
             <div class="d-flex justify-content-end mb-4">
-                <a href="{{ secure_url('/admin/categories/create') }}" class="btn btn-danger">Add category</a>
+                <a href="{{ url('/admin/categories/create') }}" class="btn btn-danger">Add category</a>
             </div>
             <div class="bg-secondary rounded p-4 mt-3">
-                <h6 class="mb-4">categories Table</h6>
+                <h6 class="mb-4">Categories Table</h6>
                 <table class="table table-dark">
                     <thead>
                         <tr>
@@ -25,9 +24,9 @@
                             <th scope="row">{{ $category->id }}</th>
                             <td>{{ $category->name_category }}</td> <!-- Assurez-vous que votre modèle category a un attribut name -->
                             <td class="text-end">
-                                <a href="{{ secure_url('/admin/categories/show', $category->id) }}" class="btn btn-primary btn-sm">Show</a>
-                                <a href="{{ secure_url('/admin/categories/edit', $category->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                <form action="{{ secure_url('/admin/categories/destroy', $category->id) }}" method="POST" style="display: inline;">
+                                <a href="{{ url('/admin/categories/' . $category->id) }}" class="btn btn-primary btn-sm">Show</a>
+                                <a href="{{ url('/admin/categories/' . $category->id . '/edit') }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ url('/admin/categories/' . $category->id) }}" method="POST" style="display: inline;">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-danger btn-sm">Delete</button>
