721037f20e8bd20681ccd828941ed9183445f5ad
saad mouden <moudensaad03@gmail.com>
2024-05-30 17:49:15 +0200

    fix

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.base')
 
 @section('content')
-    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <div class="card text-center">
-            <div class="card-body text-danger">
-                <h3 class="card-title">Welcome to the Smile Shop Admin Dashboard</h3>
-            </div>
+<div class="d-flex justify-content-center align-items-center" style="height: calc(100vh - 56px);">
+    <div class="card text-center">
+        <div class="card-body text-danger">
+            <h3 class="card-title">Welcome to the Smile Shop Admin Dashboard</h3>
         </div>
     </div>
+</div>
 @endsection
