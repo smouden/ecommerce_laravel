124475c6093b6a2c0b6d9ba77ff77187cc555ef0
saad mouden <moudensaad03@gmail.com>
2024-05-30 16:28:16 +0200

    fix

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,17 +1,16 @@
 @extends('admin.base')
 
-
 @section('content')
 <div class="container-fluid pt-4 px-4">
     <div class="row d-flex justify-content-center">
         <div class="col-sm-12 col-md-8 col-lg-6">
             <div class="bg-secondary rounded h-100 p-4">
                 <h6 class="mb-4">Modify category</h6>
-                <form action="{{ secure_url('/admin/categories/update', $category->id) }}" method="POST">
+                <form action="{{ url('/admin/categories/' . $category->id) }}" method="POST">
                     @csrf
                     @method('PUT') <!-- Important pour la mise à jour -->
                     <div class="mb-3">
-                        <label for="name_category" class="form-label">Name category </label>
+                        <label for="name_category" class="form-label">Name category</label>
                         <input type="text" class="form-control" id="name_category" name="name_category" value="{{ $category->name_category }}">
                     </div>
                     <button type="submit" class="btn btn-primary">Update</button>
