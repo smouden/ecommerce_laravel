5f90caca6ea150fdb7d3070e9089a5692a031e71
saad mouden <moudensaad03@gmail.com>
2024-05-30 16:34:41 +0200

    fix

--- resources/views/admin/brands/index.blade.php
+++ resources/views/admin/brands/index.blade.php
@@ -1,16 +1,15 @@
 @extends('admin.base')
 
-
 @section('content')
 <!-- Table Start -->
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
         <div class="col-12">
             <div class="d-flex justify-content-end mb-4">
-                <a href="{{ secure_url('/admin/brands/create') }}" class="btn btn-danger">Add brand</a>
+                <a href="{{ url('/admin/brands/create') }}" class="btn btn-danger">Add brand</a>
             </div>
             <div class="bg-secondary rounded p-4 mt-3">
-                <h6 class="mb-4">brands Table</h6>
+                <h6 class="mb-4">Brands Table</h6>
                 <table class="table table-dark">
                     <thead>
                         <tr>
@@ -25,9 +24,9 @@
                             <th scope="row">{{ $brand->id }}</th>
                             <td>{{ $brand->name_brand }}</td> <!-- Assurez-vous que votre modèle brand a un attribut name -->
                             <td class="text-end">
-                                <a href="{{ secure_url('/admin/brands/show', $brand->id) }}" class="btn btn-primary btn-sm">Show</a>
-                                <a href="{{ secure_url('/admin/brands/edit', $brand->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                <form action="{{ secure_url('/admin/brands/destroy', $brand->id) }}" method="POST" style="display: inline;">
+                                <a href="{{ url('/admin/brands/' . $brand->id) }}" class="btn btn-primary btn-sm">Show</a>
+                                <a href="{{ url('/admin/brands/' . $brand->id . '/edit') }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ url('/admin/brands/' . $brand->id) }}" method="POST" style="display: inline;">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-danger btn-sm">Delete</button>
